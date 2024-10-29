8f3fce8752e083942911ec177f6990c9076f537d
HarshitRaul24 <harshitraul15@gmail.com>
2024-10-29 23:04:55 +0530

    Changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
-const Database_URL = "mongodb://127.0.0.1:27017/MoonPayx?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1";
+// const Database_URL = "mongodb://127.0.0.1:27017/MoonPayx?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.1";
+const Database_URL = "mongodb+srv://root:root@cluster0.ca2cg9i.mongodb.net/Moonpay?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose.connect(Database_URL).then(()=>console.log('!!MongoDB Connected!!')).catch((err)=> console.log(err.message));
